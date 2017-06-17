f4990756c2da5fa3229cde7da6439e1916a24c18
guledali <guled.ali01@gmail.com>
2017-06-17 11:26:36 +0200

    changed sql fil

--- php-hemsida/php-hemsida.sql
+++ php-hemsida/php-hemsida.sql
@@ -14,32 +14,13 @@ CREATE TABLE `messages` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `messages` (`id`, `name`, `email`, `message`) VALUES
-(6,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(7,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(8,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(9,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(10,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(11,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(12,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(13,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(14,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(15,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(16,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(17,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(18,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(19,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(20,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(21,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(22,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(23,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'hej'),
-(24,	'Guled Abdikarim Ali',	'anna-erik@mail.com',	'heeelo'),
-(25,	'Guled Abdikarim Ali',	'anna-erik@mail.com',	'heeelo'),
-(26,	'Guled Abdikarim Ali',	'anna-erik@mail.com',	'hejdå'),
-(27,	'Guled Abdikarim Ali',	'anna-erik@mail.com',	'hejdå'),
-(28,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'aaddiuuuu'),
-(29,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'aaddiuuuu'),
-(30,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'aaddiuuuu'),
-(31,	'Guled Abdikarim Ali',	'guled.ali01@gmail.com',	'aaddiuuuu');
 
--- 2017-06-17 00:32:29
+DROP TABLE IF EXISTS `page`;
+CREATE TABLE `page` (
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `page` (`content`) VALUES
+(' <section class=\"mt-5\">\r\n                <div class=\"container\">\r\n            <div class=\"accordion\">\r\n                <ul class=\"list-unstyled\">\r\n                    <li class=\"open js-question-wrapper\">\r\n                        <div class=\"question\">\r\n                            <h2>Utbildningar</h2>\r\n                            <i class=\"fa fa-arrow-down\" aria-hidden=\"true\"></i>\r\n                        </div>\r\n                        <div class=\"answer\">\r\n                            <p>Har gått i flera skolor runt om  landet, gymnasieskolor har varit primärt didaktus men ett kort tag innan studerade jag i huddinge gymnasiet, efter de var de ett halvår hällefors där jag gick på en folkhögskola med inriktning industridesign. Sedan kom jag in på lärarprogrammet i dalarna. Vilket jag hoppade väldigt fort läste sedan fristående kurser i stockholms universitet. </p>\r\n                        </div>\r\n                    </li>\r\n                    <li class=\"js-question-wrapper\">\r\n                        <div class=\"question\">\r\n                            <h2>Arbete</h2>\r\n                            <i class=\"fa fa-arrow-down\" aria-hidden=\"true\"></i>\r\n                        </div>\r\n                        <div class=\"answer\">\r\n                            <p>Jag har jobbat som telefonförsäljare, jag har praktisserat i coop 3 månader och sen har haft student jobb som att sortera poster.</p>\r\n                        </div>\r\n                    </li>\r\n                    <li class=\"js-question-wrapper\">\r\n                        <div class=\"question\">\r\n                            <h2>Fritiden</h2>\r\n                            <i class=\"fa fa-arrow-down\" aria-hidden=\"true\"></i>\r\n                        </div>\r\n                        <div class=\"answer\">\r\n                            <p>Jag gör inte så mycket under fritiden läser gärna neil gaiman om jag har tid. Älskar att läsa serier och lyssna på rapmusik</p>\r\n                        </div>\r\n                    </li>\r\n                </ul>\r\n            </div>\r\n        </div>\r\n    </section>\r\n');
+
+-- 2017-06-17 09:24:54
